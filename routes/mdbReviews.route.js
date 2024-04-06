cf44f9b4ab5fcdca40f31d5565ec534d0eeb06e4
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-06 11:40:36 +0530

    add routes to delReview function

--- routes/mdbReviews.route.js
+++ routes/mdbReviews.route.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const jwtMiddleware = require("../middlewares/jwt.middleware")
-const { getReview, setReview } = require("../controllers/mdbReviews.controller")
+const { getReview, setReview, delReview } = require("../controllers/mdbReviews.controller")
 
 const mdbReviewsRouter = express.Router()
 
 mdbReviewsRouter.get("/:type/:tmdbId/review",jwtMiddleware, getReview)
 mdbReviewsRouter.post("/:type/:tmdbId/review/add", jwtMiddleware, setReview)
+mdbReviewsRouter.delete("/:type/:tmdbId/review/del", jwtMiddleware, delReview)
 
 module.exports = mdbReviewsRouter
